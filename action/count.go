ec570bc4b28e5286db8d438c745b812f79a1d4c8
515hikaru <12kojima.takahiro@gmail.com>
2019-03-31 01:24:53 +0900

    Sort by count and add right space padding

--- action/count.go
+++ action/count.go
@@ -2,9 +2,16 @@ package action
 
 import (
 	"fmt"
+	"sort"
+	"strings"
 
 	"github.com/515hikaru/mhugo/parser"
 )
+
+type tc struct {
+	Tag   string
+	Count int
+}
 
 func countTag(m parser.PairTitleAndTags) parser.PairTagAndCount {
 	countTagMap := make(parser.PairTagAndCount)
@@ -16,9 +23,28 @@ func countTag(m parser.PairTitleAndTags) parser.PairTagAndCount {
 	return countTagMap
 }
 
+func sortCount(c parser.PairTagAndCount) ([]tc, int) {
+	var tcs []tc
+	var maxLength int
+	for k, v := range c {
+		if len(k) >= maxLength {
+			maxLength = len(k)
+		}
+		t := tc{Tag: k, Count: v}
+		tcs = append(tcs, t)
+	}
+	sort.Slice(tcs, func(i, j int) bool {
+		return tcs[i].Count > tcs[j].Count
+	})
+	return tcs, maxLength
+}
+
 func PrintTags(m parser.PairTitleAndTags) {
 	c := countTag(m)
-	for k, v := range c {
-		fmt.Printf("%s:\t%d\n", k, v)
+	sorted, width := sortCount(c)
+	for _, t := range sorted {
+		diff := width - len(t.Tag)
+		interval := strings.Repeat(" ", diff)
+		fmt.Printf("%s%s\t%d\n", t.Tag, interval, t.Count)
 	}
 }

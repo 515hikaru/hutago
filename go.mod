9b6ffa805de5c91d50edf220560f4558e8573581
515hikaru <12kojima.takahiro@gmail.com>
2019-12-01 12:36:37 +0900

    initialize with cobra

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/515hikaru/hutago
 
 go 1.13
 
-require gopkg.in/yaml.v2 v2.2.7
+require (
+	github.com/spf13/cobra v0.0.5
+	gopkg.in/yaml.v2 v2.2.7
+)
--- main.go
+++ main.go
@@ -1,46 +1,7 @@
 package main
 
-import (
-	"flag"
-	"fmt"
-	"os"
-
-	"github.com/515hikaru/hutago/action"
-	"github.com/515hikaru/hutago/loader"
-	"github.com/515hikaru/hutago/parser"
-)
-
-const (
-	failCode    = 1
-	successCode = 0
-)
-
-func run() {
-	recursive := flag.Bool("r", false, "search directories and their contents recursively")
-	flag.Parse()
-	targetDirectory := flag.Arg(0)
-	if targetDirectory == "" {
-		targetDirectory = "content"
-		*recursive = true
-	}
-	fileNames, err := loader.ListDirectoryContents(targetDirectory, *recursive)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
-		os.Exit(failCode)
-	}
-	if len(fileNames) == 0 {
-		fmt.Fprintf(os.Stderr, "%s has no markdown files.\n", targetDirectory)
-		os.Exit(failCode)
-	}
-	mmap, err := parser.CreateHeaders(fileNames, targetDirectory)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
-		os.Exit(failCode)
-	}
-	action.PrintTags(mmap)
-}
+import "github.com/515hikaru/hutago/cmd"
 
 func main() {
-	run()
-	os.Exit(successCode)
+  cmd.Execute()
 }
